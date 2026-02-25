206d3388856f867bf8bea1a453d63ffb43aaaefa
Adrian Comanescu <adrian.comanescu@computacenter.com>
2026-02-25 18:02:00 +0200

    chore: ignore node_modules

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,12 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+    selector: 'app-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.scss'],
+    standalone: false
+})
+export class CardComponent   {
+
+  constructor() {}
+}
